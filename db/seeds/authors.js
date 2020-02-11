9e3984041eb01cb1a3c10e43759765ddf1c6195d
Kasumy1215 <kasumy1215@gmail.com>
2020-02-12 03:12:27 +0900

    initial commit

--- db/seeds/authors.js
+++
@@ -1,9 +0,0 @@
-const authorsJSON = require("./data/authors.json");
-
-exports.seed = function(knex, Promise) {
-  return knex("authors")
-    .del()
-    .then(() => {
-      return knex("authors").insert(authorsJSON);
-    });
-};
